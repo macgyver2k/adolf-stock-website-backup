48c28ea3fd4ba68e1b71da3107f108ff9ae23267
Christopher Kloß <christopher.kloss@ubimidum.de>
2024-03-18 00:29:03 +0100

    Added all scraped files

---
+++ scrape.sh
@@ -0,0 +1,10 @@
+wget \
+     --recursive \
+     --level 10 \
+     --page-requisites \
+     --adjust-extension \
+     --span-hosts \
+     --convert-links \
+     --restrict-file-names=windows \
+     --domains www.adolfstock.de,parastorage.com,wixstatic.com \
+         www.adolfstock.de
